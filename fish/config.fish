4133c5acf70cac15ca59ac2a05d9bcc48c53cef3
tharada <harada.tomoyuki03@gmail.com>
2018-04-08 09:53:18 +0900

    pyenv configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,6 @@
 set -g theme_display_hg yes
 set -g theme_color_scheme base16
+
+set -x PATH "/home/tharada/.pyenv/bin" $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
