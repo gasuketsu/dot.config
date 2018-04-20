33d513b7e8e40a0cacf905e4e4e350ba1189dcdf
tharada <harada.tomoyuki03@gmail.com>
2018-04-20 09:51:52 +0900

    pyenv directory validation

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 set -g theme_display_hg yes
 set -g theme_color_scheme base16
 
-set -x PATH "/home/tharada/.pyenv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
-status --is-login; and status --is-interactive; and exec byobu-launcher
+if test -d $HOME/.pyenv/bin
+    set -x PATH "$HOME/.pyenv/bin" $PATH
+    status --is-interactive; and . (pyenv init -|psub)
+    status --is-interactive; and . (pyenv virtualenv-init -|psub)
+    status --is-login; and status --is-interactive; and exec byobu-launcher
+end
