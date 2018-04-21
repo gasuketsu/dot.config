1afed43e5ae7cedfe1e0a38781cbd8d03f51cf57
tharada <harada.tomoyuki03@gmail.com>
2018-04-21 21:43:34 +0900

    remove byobu configuration

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,4 @@ if test -d $HOME/.pyenv/bin
     set -x PATH "$HOME/.pyenv/bin" $PATH
     status --is-interactive; and . (pyenv init -|psub)
     status --is-interactive; and . (pyenv virtualenv-init -|psub)
-    status --is-login; and status --is-interactive; and exec byobu-launcher
 end
