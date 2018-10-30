801c9cadda022500c6ba026d330c2924e816f2f5
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2018-10-30 09:04:31 +0900

    [fish] pyenv configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,7 @@
 set -g theme_display_hg yes
 set -g theme_color_scheme base16
+
+if test -d $HOME/.pyenv
+    set -gx PYENV_ROOT $HOME/.pyenv
+    status --is-interactive; and source (pyenv init -|psub)
+end
