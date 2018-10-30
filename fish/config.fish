3e3c2b499119284738ab596c32dc064059cd4e47
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2018-10-30 17:21:34 +0900

    [fish] improve pyenv configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 set -g theme_display_hg yes
 set -g theme_color_scheme base16
 
-if test -d $HOME/.pyenv
-    set -gx PYENV_ROOT $HOME/.pyenv
-    status --is-interactive; and source (pyenv init -|psub)
+if status --is-login
+    if test -d $HOME/.pyenv
+        set -x PYENV_ROOT $HOME/.pyenv
+        source (pyenv init -|psub)
+        source (pyenv virtualenv-init -|psub)
+    end
 end
