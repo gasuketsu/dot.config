586c050d50dac829153c736aeca47d16d8adfb69
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2018-11-02 12:30:10 +0900

    [fish] setup pyenv environment in interactive shell

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,10 @@ set -g theme_display_hg yes
 set -g theme_color_scheme base16
 set -g theme_date_format "+%Y-%m-%dT%T%:z"
 
-if status --is-login
-  if test -d $HOME/.pyenv
+if status --is-interactive
+  if test -d $HOME/.pyenv/bin
     set -x PYENV_ROOT $HOME/.pyenv
+    set -x PATH $HOME/.pyenv/bin $PATH
     source (pyenv init -|psub)
     source (pyenv virtualenv-init -|psub)
   end
