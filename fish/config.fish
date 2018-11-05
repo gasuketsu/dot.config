2212a34ba7566ec4b573dce6db14d7325996c1d7
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2018-11-05 12:53:11 +0900

    [fish] fix pyenv configuration

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,10 @@ set -g theme_display_hg yes
 set -g theme_color_scheme base16
 set -g theme_date_format "+%Y-%m-%dT%T%:z"
 
-if status --is-interactive
-  if test -d $HOME/.pyenv/bin
-    set -x PYENV_ROOT $HOME/.pyenv
-    set -x PATH $HOME/.pyenv/bin $PATH
-    source (pyenv init -|psub)
-    source (pyenv virtualenv-init -|psub)
-  end
+# pyenv
+if test -d $HOME/.pyenv/bin
+  status --is-login; and set -gx PYENV_ROOT $HOME/.pyenv
+  status --is-login; and set -gx PATH $PYENV_ROOT/bin $PATH
+  status --is-interactive; and source (pyenv init -|psub)
+  status --is-interactive; and source (pyenv virtualenv-init -|psub)
 end
