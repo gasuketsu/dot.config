1e2acd8c02d9aaf0883848c2c7fa5db5161cfb31
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2018-11-23 14:43:34 +0900

    [fish] default EDITOR configuration

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ set -g theme_display_hg yes
 set -g theme_color_scheme dark
 set -g theme_date_format "+%Y-%m-%dT%T%:z"
 
+if status --is-interactive
+  type nvim > /dev/null 2>&1; and set -gx EDITOR nvim
+end
+
 # asdf
 if test -f $HOME/.asdf/asdf.fish
   status --is-interactive; and source $HOME/.asdf/asdf.fish
