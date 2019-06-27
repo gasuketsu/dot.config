47100f306801c75853a401422134984e02451234
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2019-06-27 23:36:08 +0900

    style(fish): set fish_color

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,28 @@ set -g theme_display_hg yes
 set -g theme_color_scheme terminal
 set -g theme_date_format "+%Y-%m-%dT%T%:z"
 
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
 if status --is-interactive
     type nvim >/dev/null 2>&1
     and set -gx EDITOR nvim
