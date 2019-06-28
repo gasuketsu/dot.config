5c8f503db13e9c7686a3469f6a192274caa587f6
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2019-06-28 11:42:46 +0900

    style(fish): tweak fish_color

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ set -g theme_date_format "+%Y-%m-%dT%T%:z"
 set -g fish_color_autosuggestion '555'  'brblack'
 set -g fish_color_cancel -r
 set -g fish_color_command --bold
-set -g fish_color_comment red
+set -g fish_color_comment yellow
 set -g fish_color_cwd green
 set -g fish_color_cwd_root red
 set -g fish_color_end brmagenta
@@ -17,12 +17,13 @@ set -g fish_color_match --background=brblue
 set -g fish_color_normal normal
 set -g fish_color_operator bryellow
 set -g fish_color_param cyan
-set -g fish_color_quote yellow
+set -g fish_color_quote brgreen
 set -g fish_color_redirection brblue
 set -g fish_color_search_match 'bryellow'  '--background=brblack'
 set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
 set -g fish_color_user brgreen
 set -g fish_color_valid_path --underline
+set -g fish_color_status red
 
 if status --is-interactive
     type nvim >/dev/null 2>&1
