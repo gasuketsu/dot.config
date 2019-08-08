29bdc32cc0c9e7bf397b83cbd97dcdcfee443f05
tharada <harada.tomoyuki03@gmail.com>
2019-08-08 10:00:02 +0900

    style(fish): gruvbox theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 # bobthefish theme
 set -g theme_display_hg yes
-set -g theme_color_scheme terminal
-set -g theme_date_format "+%Y-%m-%dT%T%:z"
+set -g theme_color_scheme gruvbox
 
 # color
 set -g fish_color_autosuggestion '555'  'brblack'
@@ -44,3 +43,6 @@ if status --is-interactive
         source $HOME/.asdf/asdf.fish
     end
 end
+
+# disable right_prompt
+function fish_right_prompt; end
