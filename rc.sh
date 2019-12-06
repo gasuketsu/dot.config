99012497d069661d98ae30dfb9269900067496d4
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2019-12-06 21:05:35 +0900

    feat(rc): enable fzf in shell

--- rc.sh
+++ rc.sh
@@ -2,10 +2,15 @@ export TERM=xterm-256color
 export EDITOR="nvim"
 
 if [ -d $HOME/.local/bin ]; then
-  export PATH=$HOME/.local/bin:$PATH
+    export PATH=$HOME/.local/bin:$PATH
+fi
+
+# fzf
+if [ -f $HOME/.fzf.bash ]; then
+    source $HOME/.fzf.bash
 fi
 
 # asdf
 if [ -f $HOME/.asdf/asdf.sh ]; then
-  source $HOME/.asdf/asdf.sh
+    source $HOME/.asdf/asdf.sh
 fi
