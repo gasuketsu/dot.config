66535b92ca33debfd5b036d5804860a3ca1ec470
tharada <harada.tomoyuki03@gmail.com>
2020-02-07 00:34:45 +0000

    feat(rc): do not source fzf.bash
    user who want to use fzf in bash can source fzf.bash in your .bashrc

--- rc.sh
+++ rc.sh
@@ -5,11 +5,6 @@ if [ -d $HOME/.local/bin ]; then
     export PATH=$HOME/.local/bin:$PATH
 fi
 
-# fzf
-if [ -f $HOME/.fzf.bash ]; then
-    source $HOME/.fzf.bash
-fi
-
 # asdf
 if [ -f $HOME/.asdf/asdf.sh ]; then
     source $HOME/.asdf/asdf.sh
