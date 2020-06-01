2b86a24b04cb84daf619e39b7c46e0c50561059f
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2020-06-01 15:52:52 +0900

    refactor(tmux): refactor tmuxrc.sh

--- tmux/tmuxrc.sh
+++ tmux/tmuxrc.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Linux" ]; then
-    if [ $(uname -r | grep -i "microsoft") != "" ]; then
-        # WSL
-        if type win32yank.exe > /dev/null; then
-            tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "cat | win32yank.exe -i"
-        else
-            tmux bind-key -T copy-mode-vi y send -X copy-selection
-        fi
+function set_wsl_keybindings () {
+    if type win32yank.exe > /dev/null; then
+        tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "cat | win32yank.exe -i"
     else
-        # Native Linux
-        if type lemonade > /dev/null; then
-            tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "lemonade copy"
-        else
-            tmux bind-key -T copy-mode-vi y send -X copy-selection
-        fi
+        tmux bind-key -T copy-mode-vi y send -X copy-selection
     fi
-elif [ "$(uname)" == "Darwin" ]; then
+}
+
+function set_linux_key_bindings () {
+    if type lemonade > /dev/null; then
+        tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "lemonade copy"
+    else
+        tmux bind-key -T copy-mode-vi y send -X copy-selection
+    fi
+}
+
+function set_macos_keybindings () {
     tmux bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
-fi
+}
+
+function set_default_keybindings () {
+    tmux bind-key -T copy-mode-vi y send -X copy-selection
+}
+
+
+case "$(uname)" in
+    "Linux")
+        if [ $(uname -r | grep -i "microsoft") != "" ]; then
+            set_wsl_keybindings
+        else
+            set_linux_keybindings
+        fi
+        ;;
+    "Darwin")
+        set_macos_keybindings
+        ;;
+    *)
+        set_default_keybindings
+esac
