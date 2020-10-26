cae2f7c2fdf71272f7b55c16c42514922be1654c
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2020-10-27 08:25:56 +0900

    fix(setup): use $HOME instead of ~

--- setup.sh
+++ setup.sh
@@ -23,9 +23,9 @@ golang_default_packages=(
 function setup_neovim_clipboard_config () {
   # set .nvimrc_local if no clipboard configuration
   if [ "$(uname -r | grep -i "microsoft")" != "" ] && type win32yank.exe > /dev/null; then
-    if [ ! -f ~/.nvimrc_local ] || ! grep 'let g:clipboard' ~/.nvimrc_local > /dev/null 2>&1; then
+    if [ ! -f "$HOME/.nvimrc_local" ] || ! grep 'let g:clipboard' "$HOME/.nvimrc_local" > /dev/null 2>&1; then
       echo "#### setting up neovim clipboard to use win32yank..."
-      cat <<EOT >> ~/.nvimrc_local
+      cat <<EOT >> "$HOME/.nvimrc_local"
 let g:clipboard = {
       \\   'name': 'win32yank',
       \\   'copy': {
@@ -45,68 +45,68 @@ EOT
 
 
 # vim-plug (for neovim)
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+  curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Fisherman
-if [ ! -f ~/.config/fish/functions/fisher.fish ]; then
-  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+if [ ! -f "$HOME/.config/fish/functions/fisher.fish" ]; then
+  curl -Lo "$HOME/.config/fish/functions/fisher.fish" --create-dirs https://git.io/fisher
 fi
 
 # tpm
-if [ ! -d ~/.config/tmux/plugins/tpm ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm"
 fi
 
 # clang-format
-if [ ! -e ~/.clang-format ]; then
-  ln -s ~/.config/clang-format/clang-format ~/.clang-format
+if [ ! -e "$HOME/.clang-format" ]; then
+  ln -s "$HOME/.config/clang-format/clang-format" $"HOME/.clang-format"
 fi
 
 # asdf
-if [ ! -d ~/.asdf ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+if [ ! -d "$HOME/.asdf" ]; then
+  git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf"
 fi
 
-if [ ! -e ~/.config/fish/completions/asdf.fish ]; then
-  mkdir -p ~/.config/fish/completions
-  ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+if [ ! -e "$HOME/.config/fish/completions/asdf.fish" ]; then
+  mkdir -p "$HOME/.config/fish/completions"
+  ln -s "$HOME/.asdf/completions/asdf.fish" "$HOME/.config/fish/completions"
 fi
 
 # EditorConfig
-if [ ! -e ~/.editorconfig ]; then
-  ln -s ~/.config/editorconfig/editorconfig ~/.editorconfig
+if [ ! -e "$HOME/.editorconfig" ]; then
+  ln -s "$HOME/.config/editorconfig/editorconfig" "$HOME/.editorconfig"
 fi
 
 # default python packages (asdf-python)
-if [ ! -e ~/.default-python-packages ]; then
-  ln -s ~/.config/asdf/default-python-packages ~/.default-python-packages
+if [ ! -e "$HOME/.default-python-packages" ]; then
+  ln -s "$HOME/.config/asdf/default-python-packages" "$HOME/.default-python-packages"
 fi
 
 # .gitconfig
-if [ ! -e ~/.gitconfig ]; then
-  touch ~/.gitconfig
+if [ ! -e "$HOME/.gitconfig" ]; then
+  touch "$HOME/.gitconfig"
 fi
 
 # lemonade
-if [ ! -f ~/.config/lemonade.toml ]; then
-  cp ~/.config/lemonade/default.toml ~/.config/lemonade.toml
+if [ ! -f "$HOME/.config/lemonade.toml" ]; then
+  cp "$HOME/.config/lemonade/default.toml" "$HOME/.config/lemonade.toml"
 fi
 
-source "~/.config/bash/config.bash"
+source "$HOME/.config/bash/config.bash"
 
 # update asdf to latest stable release
 asdf update
 
 # install tools
-if [ -d ~/.asdf ]; then
+if [ -d "$HOME/.asdf" ]; then
   for plugin in "${asdf_plugins[@]}"; do
     echo "#### (asdf) installing plugin $plugin ..."
     asdf plugin-add $plugin
     if [ $plugin = "nodejs" ]; then
       # Import the Node.js release team's OpenPGP keys
-      bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+      bash "$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring"
     fi
     version=$(asdf latest $plugin)
     echo "#### (asdf) installing $plugin $version..."
@@ -120,17 +120,17 @@ if [ -d ~/.asdf ]; then
 fi
 
 # (python) venv for nvim python bindings
-cd ~/.config/nvim/py3nvim
+cd "$HOME/.config/nvim/py3nvim"
 echo "##### (python) reinitialize virtualenv for neovim python bindings..."
 pipenv --rm
 pipenv install
 
 py3nvim_venv=`pipenv --venv`
-if [ ! -f ~/.nvimrc_local ]; then
-  echo "let g:python3_host_prog = "\'"${py3nvim_venv}/bin/python"\' >> ~/.nvimrc_local
+if [ ! -f "$HOME/.nvimrc_local" ]; then
+  echo "let g:python3_host_prog = "\'"${py3nvim_venv}/bin/python"\' >> "$HOME/.nvimrc_local"
 else
-  if ! grep 'let g:python3_host_prog' ~/.nvimrc_local > /dev/null 2>&1; then
-    echo "let g:python3_host_prog = "\'"${py3nvim_venv}/bin/python"\' >> ~/.nvimrc_local
+  if ! grep 'let g:python3_host_prog' "$HOME/.nvimrc_local" > /dev/null 2>&1; then
+    echo "let g:python3_host_prog = "\'"${py3nvim_venv}/bin/python"\' >> "$HOME/.nvimrc_local"
   fi
 fi
 cd $CWD
@@ -156,6 +156,7 @@ echo
 echo "######################################################"
 echo " Finished setup"
 echo
-echo " Make sure to add following line into your ~/.bashrc"
+echo " To enable configuration in bash, make sure to add"
+echo " following line into your ~/.profile or ~/.bashrc"
 echo " source ~/.config/bash/config.bash"
 echo "######################################################"
