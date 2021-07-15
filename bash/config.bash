dea98f6dbb78cf4b4525869f9717e41f9a9fecb8
Tomoyuki Harada <harada.tomoyuki03@gmail.com>
2021-07-15 10:55:18 +0900

    chore(config): Do not source .fzf.bash

--- bash/config.bash
+++ bash/config.bash
@@ -5,8 +5,5 @@ if [ -d $HOME/.local/bin ] && ! echo $PATH | grep -q "$HOME/.local/bin"; then
   export PATH=$HOME/.local/bin:$PATH
 fi
 
-# fzf
-[ -f "$HOME/.fzf.bash" ] && source "$HOME/.fzf.bash"
-
 # asdf
 [ -f "$HOME/.asdf/asdf.sh" ] && source "$HOME/.asdf/asdf.sh"
