976ab898c8cda9ef793c25e927da164763725faf
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2021-11-27 08:29:59 +0900

    Revert "Revert "chore(neovim): switch LSP and Completion plugin""
    This reverts following commits:
    bcc50c1
    e8a494e
    ed80f9e
    d8f58ce
    74aa329
    ec864eb

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,1 +1,9 @@
 require("bufferline").setup()
+
+local function buf_set_keymap(...)
+	vim.api.nvim_buf_set_keymap(bufnr, ...)
+end
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "]b", [[<Cmd>BufferLineCycleNext<CR>]], opts)
+vim.api.nvim_set_keymap("n", "[b", [[<Cmd>BufferLineCyclePrev<CR>]], opts)
