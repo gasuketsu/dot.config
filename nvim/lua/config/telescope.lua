a688045f3d0889421ad92997a9ee1ddfa9d01198
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2021-12-06 10:19:21 +0900

    chore(neovim): tweak telescope keymap

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,8 +1,13 @@
+local actions = require("telescope.actions")
 require("telescope").setup({
 	defaults = {
 		mappings = {
 			n = {
-				["D"] = "delete_buffer",
+				["d"] = actions.delete_buffer,
+			},
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
 			},
 		},
 	},
