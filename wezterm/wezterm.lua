99dff23f3cdb7846a46dcf3ced328e6f69102a7a
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2021-12-23 10:41:46 +0900

    chore(wezterm): tweak launch menu

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,51 @@
 local wezterm = require("wezterm")
 
+local launch_menu = {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  table.insert(launch_menu, {
+    label = "PowerShell",
+    args = { "powershell.exe", "-NoLogo" },
+  })
+
+  -- Enumerate any WSL distributions that are installed and add those to the menu
+  local success, wsl_list, wsl_err = wezterm.run_child_process({ "wsl.exe", "-l" })
+  -- `wsl.exe -l` has a bug where it always outputs utf16:
+  -- https://github.com/microsoft/WSL/issues/4607
+  -- So we get to convert it
+  wsl_list = wezterm.utf16_to_utf8(wsl_list)
+
+  for idx, line in ipairs(wezterm.split_by_newlines(wsl_list)) do
+    -- Skip the first line of output; it's just a header
+    if idx > 1 then
+      -- Remove the "(Default)" marker from the default line to arrive
+      -- at the distribution name on its own
+      local distro = line:gsub(" %(Default%)", "")
+
+      -- Add an entry that will spawn into the distro with the default shell
+      table.insert(launch_menu, {
+        label = distro .. " (WSL default shell)",
+        args = { "wsl.exe", "--distribution", distro },
+      })
+
+      -- Here's how to jump directly into some other program; in this example
+      -- its a shell that probably isn't the default, but it could also be
+      -- any other program that you want to run in that environment
+      table.insert(launch_menu, {
+        label = distro .. " (WSL zsh login shell)",
+        args = { "wsl.exe", "--distribution", distro, "--exec", "/bin/zsh", "-l" },
+      })
+    end
+  end
+end
+
 return {
   initial_rows = 40,
   initial_cols = 180,
 
   default_prog = { "wsl", "~" },
+  launch_menu = launch_menu,
+
   font = wezterm.font_with_fallback({ "PlemolJP Console NF" }),
   font_size = 12,
   window_background_opacity = 0.95,
