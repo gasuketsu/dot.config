c33ded555b11e7a8a6536316ed4a9b20e8c71674
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-03-31 11:17:50 +0900

    chore(wezterm): remove adding WSL distros to menu list manually

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,28 +7,6 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     label = "PowerShell",
     args = { "powershell.exe", "-NoLogo" },
   })
-
-  -- Enumerate any WSL distributions that are installed and add those to the menu
-  local success, wsl_list, wsl_err = wezterm.run_child_process({ "wsl.exe", "-l" })
-  -- `wsl.exe -l` has a bug where it always outputs utf16:
-  -- https://github.com/microsoft/WSL/issues/4607
-  -- So we get to convert it
-  wsl_list = wezterm.utf16_to_utf8(wsl_list)
-
-  for idx, line in ipairs(wezterm.split_by_newlines(wsl_list)) do
-    -- Skip the first line of output; it's just a header
-    if idx > 1 then
-      -- Remove the "(Default)" marker from the default line to arrive
-      -- at the distribution name on its own
-      local distro = line:gsub(" %(既定%)", "")
-
-      -- Add an entry that will spawn into the distro with the default shell
-      table.insert(launch_menu, {
-        label = distro,
-        args = { "wsl.exe", "--distribution", distro },
-      })
-    end
-  end
 end
 
 return {
