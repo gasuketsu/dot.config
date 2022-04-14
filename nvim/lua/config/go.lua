fb020412bda5dcce737f3b814b3bda876582245f
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-04-14 11:16:48 +0900

    chore(neovim): tweak golang configuration

--- nvim/lua/config/go.lua
+++
@@ -1,3 +0,0 @@
-require("go").setup({
-  gofmt = "gofmt",
-})
--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,5 +1,9 @@
 require("nvim-tree").setup({
   view = {
     auto_resize = true,
+    mappings = {
+      { key = "g?", action = "" },
+      { key = "?", action = "toggle_help" },
+    },
   },
 })
