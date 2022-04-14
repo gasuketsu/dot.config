f13ac939694e84a2330b384105c3d055fcc10419
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-04-15 08:18:43 +0900

    chore(neovim): go.nvim integrates with nvim-lsp-installer

--- nvim/lua/config/go.lua
+++ nvim/lua/config/go.lua
@@ -1,3 +1,9 @@
+local path = require("nvim-lsp-installer.path")
+local install_root_dir = path.concat({ vim.fn.stdpath("data"), "lsp_servers" })
+
 require("go").setup({
-  gofmt = "gofmt",
+  gopls_cmd = { install_root_dir .. "/go/gopls" },
+  filstruct = "gopls",
+  dap_debug = true,
+  dap_debug_gui = true,
 })
