95bc45f3e872d0b67443db2dffd0068a6d05d433
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-04-27 10:41:11 +0900

    chore(neovim): tweak keymap for completion

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -5,7 +5,7 @@ local source_map = {
   nvim_lsp = "[LSP]",
   buffer = "[Buffer]",
   path = "[Path]",
-  luasnip = "[Snippet]",
+  vsnip = "[Snippet]",
   cmdline = "[Command]",
   cmp_tabnine = "[Tabnine]",
 }
@@ -14,35 +14,31 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      require("luasnip").lsp_extend(args.body)
+      vim.fn["vsnip#anonymous"](args.body)
     end,
   },
   formatting = {
     format = lspkind.cmp_format({ with_text = true, menu = source_map, maxwidth = 50 }),
   },
   mapping = {
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-n>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item({ behavvior = cmp.ConfirmBehavior.Replace })
-      else
-        cmp.complete()
-      end
-    end, { "i", "c" }),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-y>"] = cmp.config.disable,
-    ["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
-    ["<CR>"] = cmp.mapping(
-      cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      }),
-      { "i", "c" }
-    ),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    ["<TAB>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
-    { name = "luasnip" },
+    { name = "vsnip" },
   }, {
     { name = "cmp_tabnine" },
     { name = "buffer" },
