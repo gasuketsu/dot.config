bbfddc0d0db01e5f29b75a2f1df9771d7916f276
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-06-16 20:28:13 +0900

    chore(neovim): install trouble.nvim and lsp-colors.nvim

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,3 +1,6 @@
+-- LSP-colors
+require("lsp-colors").setup()
+
 -- Language servers to be installed
 local servers = {
   gopls = {settings = {gopls = {gofumpt = true}}},
@@ -80,7 +83,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 
 -- Sign Definition
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
