1facd3e2eedd0018a16e6f44b3385b70758c1139
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-06-17 17:02:15 +0900

    chore(neovim): set max line length for golines

--- nvim/lua/config/go.lua
+++ nvim/lua/config/go.lua
@@ -4,4 +4,5 @@ local install_root_dir = path.concat({ vim.fn.stdpath("data"), "lsp_servers" })
 require("go").setup({
   gopls_cmd = { install_root_dir .. "/gopls/gopls" },
   tag_transform = "camelcase",
+  max_line_len = 100,
 })
