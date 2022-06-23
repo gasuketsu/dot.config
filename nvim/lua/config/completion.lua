ce2981df6d9758c6b496f6d37fa0b68434eed7d4
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-06-23 22:36:27 +0900

    chore(neovim): switch snippet plugin to luasnip

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -14,31 +14,37 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
   formatting = {
     format = lspkind.cmp_format({ with_text = true, menu = source_map, maxwidth = 50 }),
   },
   mapping = {
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
-    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "c"}),
-    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "c"}),
-    ["<C-y>"] = cmp.mapping(cmp.config.disable, {"i", "c"}),
-    ["<C-e>"] = cmp.mapping(cmp.mapping.abort(), {"i", "c"}),
-    ["<CR>"] = cmp.mapping(cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = false,
-    }), {"i", "c"}),
-    ["<TAB>"] = cmp.mapping(cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-    }), {"i"}),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+    ["<C-y>"] = cmp.mapping(cmp.config.disable, { "i", "c" }),
+    ["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+    ["<CR>"] = cmp.mapping(
+      cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }),
+      { "i", "c" }
+    ),
+    ["<TAB>"] = cmp.mapping(
+      cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      }),
+      { "i" }
+    ),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
-    { name = "vsnip" },
+    { name = "luasnip" },
   }, {
     { name = "cmp_tabnine" },
     { name = "buffer" },
@@ -55,7 +61,10 @@ cmp.setup.cmdline("/", {
 
 -- Source for ":" (if you enabled `native_menu`, this won"t work anymore).
 cmp.setup.cmdline(":", {
-  sources = {
-    { name = "native_menu" },
-  },
+  -- mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
