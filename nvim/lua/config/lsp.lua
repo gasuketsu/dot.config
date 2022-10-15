ceb0a815de771bffad3e97443853978ee8160335
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-10-15 23:48:05 +0900

    chore(neovim): remove deprecated function

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -61,7 +61,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities =
-  require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- setup servers and map buffer local keybindings when the language server attaches
 local lspconfig = require("lspconfig")
