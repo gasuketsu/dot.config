247facc0b1b7446a1381080801938221cc2d8d2d
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-11-27 23:23:11 +0900

    chore(neovim): switch from mini.lua to separated plugins

--- nvim/lua/config/mini.lua
+++
@@ -1,5 +0,0 @@
-require("mini.comment").setup()
-require("mini.trailspace").setup()
-require("mini.surround").setup()
-
-vim.keymap.set("n", "<A-t>", "<cmd>lua MiniTrailspace.trim()<CR>", { silent = true })
