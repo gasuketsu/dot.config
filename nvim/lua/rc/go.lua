47185c288127572dc275c77aa2bf8ee2b0b278b1
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2023-06-11 22:44:35 +0900

    feat(neovim): treat *.mod files as gomod file

--- nvim/lua/rc/go.lua
+++ nvim/lua/rc/go.lua
@@ -3,3 +3,13 @@ require("go").setup({
   formatter = "lsp",
   tags_options = {},
 })
+
+-- Treat *.mod files as gomod file
+vim.api.nvim_clear_autocmds({
+  event = { "BufRead", "BufNewFile" },
+  pattern = { "*.mod", "*.MOD" },
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.mod", "*.MOD" },
+  command = "set filetype=gomod",
+})
