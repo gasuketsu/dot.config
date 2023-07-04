5fe6dd6e9c184b62f19a79d9658e3811f66f29c5
HARADA Tomoyuki <harada.tomoyuki03@gmail>
2023-07-04 19:55:35 +0900

    chore(neovim): add null-ls source for formatting

--- nvim/lua/rc/null-ls.lua
+++ nvim/lua/rc/null-ls.lua
@@ -2,7 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.fish_indent,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.stylua,
   },
 })
