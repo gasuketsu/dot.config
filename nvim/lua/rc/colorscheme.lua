ddc21024ede33dcdfe2520e843b213a3c3b91ad6
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2023-09-07 20:20:15 +0900

    neovim: use default color palette

--- nvim/lua/rc/colorscheme.lua
+++ nvim/lua/rc/colorscheme.lua
@@ -2,11 +2,6 @@ vim.opt.termguicolors = true
 
 require("kanagawa").setup({
   keywordStyle = { italic = false },
-  colors = {
-    palette = {
-      samuraiRed = "#c34043", -- apply autumnRed
-    },
-  },
   terminalColors = false,
   theme = "wave",
   background = {
