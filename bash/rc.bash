625650c73339d155acafd14280b02d30f2558ff2
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2023-09-23 16:21:11 +0900

    tweak fzf prompt color

--- bash/rc.bash
+++ bash/rc.bash
@@ -1,6 +1,6 @@
 export EDITOR="nvim"
 export PIPENV_VENV_IN_PROJECT=1
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height 90% --layout=reverse --color=fg:#e5d5ad,bg:#282828,hl:#959c36 --color=fg+:#f4e8be,bg+:#3c3836,hl+:#b0b846 --color=info:#80aa9e,prompt:#e9b143,pointer:#f2594b --color=marker:#b36e81,spinner:#749e69,header:#665c54"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height 90% --layout=reverse --color=fg:#dcd7ba,bg:#181616,hl:#6f894e --color=fg+:#f2ecbc,bg+:#282727,hl+:#98bb6c --color=info:#7fb4ca,prompt:#e6c384,pointer:#ff5d62 --color=marker:#938aa9,spinner:#7aa89f,header:#625e5a"
 
 if [ -d $HOME/.local/bin ] && ! echo $PATH | grep -q "$HOME/.local/bin"; then
   export PATH=$HOME/.local/bin:$PATH
