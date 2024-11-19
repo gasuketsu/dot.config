82df80f456642726376fa14d309e162bbe1a69be
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2024-11-19 23:34:20 +0900

    ensure stylua installed in mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -20,6 +20,7 @@ return {
                     "gotests",
                     "iferr",
                     "impl",
+                    "stylua",
                     "yamlfmt",
                 },
             })
