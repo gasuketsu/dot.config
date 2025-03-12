bd83a41da1e47bd4869451112a2b7937679bbf01
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2025-03-12 21:55:58 +0900

    chore(neovim): define keymap for completion explicitly

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -4,17 +4,7 @@ return {
         version = "*",
         opts = {
             keymap = {
-                preset = "enter",
-                ["<C-space>"] = {},
-                ["<C-n>"] = {
-                    function(cmp)
-                        if not cmp.is_visible() then
-                            return cmp.show()
-                        end
-                    end,
-                    "select_next",
-                    "fallback_to_mappings",
-                },
+                preset = "none",
                 ["<C-i>"] = { "show", "show_documentation", "hide_documentation" },
                 ["<Esc>"] = {
                     function(cmp)
@@ -24,8 +14,25 @@ return {
                     end,
                     "fallback",
                 },
-                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+                ["<C-e>"] = { "hide", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+                ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+                ["<C-n>"] = {
+                    function(cmp)
+                        if not cmp.is_visible() then
+                            return cmp.show()
+                        end
+                    end,
+                    "select_next",
+                    "fallback_to_mappings",
+                },
+                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
                 ["<Tab>"] = { "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
+                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
             },
             cmdline = {
                 keymap = {
