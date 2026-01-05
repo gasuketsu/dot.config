ac34f92749407f62b289c411b607d4c1d71ede28
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-01-06 07:58:54 +0900

    fish: tweak syntax highlighting variables

--- fish/conf.d/20-colors.fish
+++ fish/conf.d/20-colors.fish
@@ -2,7 +2,7 @@
 
 set -l foreground DCD7BA normal
 set -l selection 2D4F67 brcyan
-set -l comment 727169 brblack
+set -l comment 54546D brblack
 set -l red C34043 red
 set -l orange FF9E64 brred
 set -l yellow C0A36E yellow
@@ -12,23 +12,23 @@ set -l cyan 7AA89F cyan
 set -l pink D27E99 brmagenta
 
 # Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $foreground --bold
-set -g fish_color_keyword $pink
-set -g fish_color_quote $green
-set -g fish_color_redirection $cyan
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $yellow
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
+set fish_color_normal $foreground
+set fish_color_command $foreground --bold
+set fish_color_keyword $pink
+set fish_color_quote $green
+set fish_color_redirection $cyan
+set fish_color_end $orange
+set fish_color_error $red
+set fish_color_param $purple
+set fish_color_comment $comment
+set fish_color_selection --background=$selection
+set fish_color_search_match --background=$selection
+set fish_color_operator $yellow
+set fish_color_escape $pink
+set fish_color_autosuggestion $comment
 
 # Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
+set fish_pager_color_progress $comment
+set fish_pager_color_prefix $cyan
+set fish_pager_color_completion $foreground
+set fish_pager_color_description $comment
