591d7e35b2b2a2bd87a0b12d2df240836a66cff5
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-01-05 22:22:34 +0900

    fish: set default keybindings

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 # suppress greeting message
 set fish_greeting
 
+set --global fish_key_bindings fish_default_key_bindings
+
 if status --is-interactive
     mise activate fish | source
     mise exec zoxide --command "zoxide init fish" | source
