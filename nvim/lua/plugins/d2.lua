d237128a27d3e9064ae022b621e10fe3dc48d47d
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-03-09 22:16:54 +0900

    neovim: add d2lang plugins

---
+++ nvim/lua/plugins/d2.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "ravsii/tree-sitter-d2",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        version = "*", -- use the latest git tag instead of main
+        build = "make nvim-install",
+    },
+    {
+        "terrastruct/d2-vim",
+        ft = { "d2" },
+    },
+}
